666c83a49161bc5dfa419da0c6baed742685b854
Baldassarre Thomas <90457907+Sushiman31@users.noreply.github.com>
2024-11-29 20:51:30 +0100

    traitement tts dernier chunk
    le tts traite maintenant le dernier chunk generé par llama

--- llama_answer.py
+++ llama_answer.py
@@ -1,9 +1,19 @@
 import ollama
-
 from gtts import gTTS
 import io
 from pydub import AudioSegment
 from pydub.playback import play
+
+def generate_tts(text):
+    myobj = gTTS(text=text, lang="en", slow=False)
+
+    mp3_buffer = io.BytesIO()
+    myobj.write_to_fp(mp3_buffer)
+    mp3_buffer.seek(0)  
+
+    # MP3 to Wav
+    audio = AudioSegment.from_file(mp3_buffer, format="mp3")       
+    play(audio)
 
 def generate_answer(transcript):
     full_transcript=[{"role":"user","content":transcript}]
@@ -22,22 +32,14 @@ def generate_answer(transcript):
         text_buffer +=chunk['message']['content']
         if text_buffer.endswith('.'):
             print(text_buffer,end="\n",flush=True)
-            
-
-            myobj = gTTS(text=text_buffer, lang="en", slow=False)
-
-            mp3_buffer = io.BytesIO()
-            myobj.write_to_fp(mp3_buffer)
-            mp3_buffer.seek(0)  
-
-            # MP3 to Wav
-            audio = AudioSegment.from_file(mp3_buffer, format="mp3")       
-            play(audio)
+        
+            generate_tts(text_buffer)
 
             full_text+=text_buffer
             text_buffer=""
     if text_buffer:
         print(text_buffer,end="\n",flush=True)
+        generate_tts(text_buffer)
         full_text+=text_buffer
     
     full_transcript.append({"role":"assistant","content":full_text})
